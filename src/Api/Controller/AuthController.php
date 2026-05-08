8ba9a56391756f519be1f63d6198abea5edc7fc7
Sonsuz Us <52765796+sonsuzus@users.noreply.github.com>
2026-05-08 23:06:34 +0300

    Update Pusher import statement in AuthController

--- src/Api/Controller/AuthController.php
+++ src/Api/Controller/AuthController.php
@@ -17,7 +17,7 @@ use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Pusher;
+use Pusher\Pusher;
 
 class AuthController implements RequestHandlerInterface
 {
